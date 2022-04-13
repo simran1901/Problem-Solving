c15761f83f04e0667455c1f38ae84e5e037c215b
Simran Makhijani <simranmakhijani55@gmail.com>
2022-04-13 09:35:39 +0530

    added nastia-and-nearly-good-numbers.cpp

---
+++ nastia-and-nearly-good-numbers.cpp
@@ -0,0 +1,32 @@
+// Codeforces
+// Author: simranmakhijani55
+//
+
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+void solve()
+{
+    ll a, b;
+    cin >> a >> b;
+    if (b == 1)
+        cout << "NO\n";
+    else
+        cout << "YES\n"
+             << a * 1ll << " " << a * b * 1ll << " " << (a * b + a) * 1ll << "\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
