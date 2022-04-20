490a7cf5542887f557168790062642c924f3010e
Simran Makhijani <simranmakhijani55@gmail.com>
2022-04-20 21:10:38 +0530

    added 688B-lovely-palindromes.cpp

---
+++ 688B-lovely-palindromes.cpp
@@ -0,0 +1,26 @@
+// Codeforces
+// Author: simranmakhijani55
+// https://codeforces.com/problemset/problem/688/B
+
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+void solve()
+{
+    string n;
+    cin >> n;
+    cout << n;
+    reverse(n.begin(), n.end());
+    cout << n;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    solve();
+
+    return 0;
+}
