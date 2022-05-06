33b49575d0eff8bd0cab739fc9ec8d2380358082
Simran Makhijani <simranmakhijani55@gmail.com>
2022-05-06 18:24:15 +0530

    added vertical-paths.cpp

---
+++ vertical-paths.cpp
@@ -0,0 +1,72 @@
+// Codeforces
+// Author: simranmakhijani55
+// https://codeforces.com/contest/1675/problem/D
+
+#include <bits/stdc++.h>
+#define ll long long int
+#define repn(i, m, n) for (int i = m; i < n; i++)
+#define rep(i, n) repn(i, 0, n)
+
+using namespace std;
+
+vector<vector<int>> ans;
+ 
+void dfs(int node, int parent, vector<int> tree[], vector<int>& path) {
+    path.push_back(node);
+    if((int)tree[node].size() == 1 and tree[node][0] == parent) {
+        ans.push_back(path);
+        return;
+    }
+    for(int nbr : tree[node]) {
+        if(nbr == parent) continue;
+        dfs(nbr, node, tree, path);
+        path.clear();
+    }
+}
+ 
+void solve() {
+    int n;
+    cin >> n;
+    ans.clear();
+    vector<int> tree[n];
+    vector<int> p(n);
+    int root = 0;
+    for(int i = 0; i < n; i++) {
+        cin >> p[i];
+        p[i]--;
+        if(i != p[i]) {
+            tree[p[i]].push_back(i);
+            tree[i].push_back(p[i]);
+        } else {
+            root = i;
+        }
+    }
+    if(n == 1) {
+        cout << "1\n1\n1\n\n";
+        return;
+    }
+    vector<int> path;
+    dfs(root, root, tree, path);
+    cout << ans.size() << '\n';
+    for(auto& f : ans) {
+        cout << f.size() << '\n';
+        for(auto& e : f) {
+            cout << e + 1 << ' ';
+        }
+        cout << '\n';
+    }
+    cout << '\n';
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+
+    return 0;
+}
