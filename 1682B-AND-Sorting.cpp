fdc2a94966973ee342249b6fc7524bb70f6ccb43
Simran Makhijani <simranmakhijani55@gmail.com>
2022-05-30 21:35:01 +0530

    added 1682B-AND-Sorting.cpp

---
+++ 1682B-AND-Sorting.cpp
@@ -0,0 +1,49 @@
+// Codeforces
+// Author: simranmakhijani55
+// https://codeforces.com/contest/1682/problem/B
+
+#include <bits/stdc++.h>
+#define ll long long int
+#define repn(i, m, n) for (ll i = m; i < n; i++)
+#define rep(i, n) repn(i, 0, n)
+#define vv vector<vector<ll>>
+#define v vector<ll>
+#define cy cout << "YES\n";
+#define cn cout << "NO\n";
+
+using namespace std;
+
+void solve()
+{
+    ll n, ans = -1;
+    cin >> n;
+    v originalArr(n);
+    rep(i, n)
+    {
+        cin >> originalArr[i];
+    }
+    v sortedArr(originalArr);
+    sort(sortedArr.begin(), sortedArr.end());
+
+    rep(i, n)
+    {
+        if (sortedArr[i] != originalArr[i])
+        {
+            ans = ans & originalArr[i];
+        }
+    }
+    cout << ans << '\n';
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+
+    return 0;
+}
