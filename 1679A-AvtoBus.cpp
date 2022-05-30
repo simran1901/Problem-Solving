5fc8920d19384ae3464b29bd2504310c6fc2cde2
Simran Makhijani <simranmakhijani55@gmail.com>
2022-05-30 21:32:35 +0530

    added 1679A-AvtoBus.cpp

---
+++ 1679A-AvtoBus.cpp
@@ -0,0 +1,45 @@
+// Codeforces
+// Author: simranmakhijani55
+// https://codeforces.com/contest/1679/problem/A
+
+#include <bits/stdc++.h>
+#define ll long long int
+#define repn(i, m, n) for (ll i = m; i < n; i++)
+#define rep(i, n) repn(i, 0, n)
+#define vv vector<vector<ll>>
+#define v vector<ll>
+#define cy cout << "YES\n";
+#define cn cout << "NO\n";
+
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    ll maxVal = 0, minVal = 0;
+    if (n % 2 || n < 4)
+    {
+        cout << -1 << "\n";
+        return;
+    }
+    // for min divide by 6
+    minVal = n / 6;
+    if (n % 6 != 0)
+        minVal++;
+    maxVal = n / 4;
+    cout << minVal << " " << maxVal << "\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+
+    return 0;
+}
