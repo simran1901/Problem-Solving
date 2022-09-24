6db866e573824d3d980ac8adcd75fdacc48426fc
Simran Makhijani <simranmakhijani55@gmail.com>
2022-09-24 23:54:03 +0530

    added kickstart round F-22

---
+++ Kickstart-22-round-F-1.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+#define IOS                       \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define ll long long int
+#define repn(i, m, n) for (ll i = m; i <= n; i++)
+#define rep(i, n) repn(i, 0, n)
+#define pern(i, m, n) for (ll i = m; i >= n; i--)
+#define per(i, n) pern(i, n, 0)
+#define vl vector<ll>
+#define vvl vector<vl>
+#define v vector<int>
+#define vv vector<v>
+#define inpArr(arr, m, n) repn(i, m, n) cin >> arr[i];
+#define printArr(arr, m, n) repn(i, m, n) cout << arr[i] << ' ';
+#define cy cout << "YES\n"
+#define cn cout << "NO\n"
+
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin>>n;
+    vector<pair<string, int>> c(n);
+    vector<pair<int,int>> d(n);
+    vector<int> u(n);
+    int temp;
+    rep(i, n-1) {
+        cin>>c[i].first>>d[i].first;
+        cin>>temp;
+        c[i].second = temp;
+        d[i].second = temp;
+    }
+    
+    sort(c.begin(), c.end());
+    sort(d.begin(), d.end());
+    int ans = 0;
+    rep(i, n-1) {
+        if (d[i].second == c[i].second) {
+            ans++;
+        }
+    }
+    cout<<ans<<'\n';
+}
+
+int main()
+{
+    IOS;
+
+    int t;
+    cin >> t;
+    repn(i, 1, t) {
+        cout<<"Case #"<<i<<": ";
+        solve();
+    }
+
+    return 0;
+}
---
+++ Kickstart-22-round-F-2.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+#define IOS                       \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define ll long long int
+#define repn(i, m, n) for (ll i = m; i <= n; i++)
+#define rep(i, n) repn(i, 0, n)
+#define pern(i, m, n) for (ll i = m; i >= n; i--)
+#define per(i, n) pern(i, n, 0)
+#define vl vector<ll>
+#define vvl vector<vl>
+#define v vector<int>
+#define vv vector<v>
+#define inpArr(arr, m, n) repn(i, m, n) cin >> arr[i];
+#define printArr(arr, m, n) repn(i, m, n) cout << arr[i] << ' ';
+#define cy cout << "YES\n"
+#define cn cout << "NO\n"
+
+using namespace std;
+
+void solve()
+{
+    ll n, q;
+    cin >> n >> q;
+
+    // build tree
+    ll x, y;
+    vector<vector<ll>> adj(n, vector<ll>());
+    for (ll i = 0; i < n - 1; i++)
+    {
+        cin >> x >> y;
+        x--, y--;
+        // if (x<y)
+        adj[x].push_back(y);
+        // else
+        adj[y].push_back(x);
+    }
+
+    ll temp;
+    for (ll i = 0; i < q; i++)
+    {
+        cin >> temp;
+    }
+    // total water = q;
+
+    queue<ll> que;
+    // cout<<q<<endl;
+
+    vector<bool> visited(n, false);
+
+    que.push(0);
+    ll ans = 0;
+    visited[0] = true;
+    while (!que.empty())
+    {
+        ll s = que.size();
+        if (s <= q)
+        {
+            ans += s;
+            q -= s;
+        }
+        else
+        {
+            break;
+        }
+        ll tempS = 0;
+        for (ll i = 0; i < s; i++)
+        {
+            ll m = que.front();
+            que.pop();
+            // traverse
+            for (ll j = 0; j < adj[m].size(); j++)
+            {
+                if (!visited[adj[m][j]])
+                {
+                    visited[adj[m][j]] = true;
+                    que.push(adj[m][j]);
+                }
+            }
+        }
+    }
+
+    cout << ans << '\n';
+}
+
+int main()
+{
+    IOS;
+
+    int t;
+    cin >> t;
+    repn(i, 1, t)
+    {
+        cout << "Case #" << i << ": ";
+        solve();
+    }
+
+    return 0;
+}
