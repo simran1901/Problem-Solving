69dd870c447c7d2371a817d3ad75588db17f8b1c
simran1901 <simranmakhijani55@gmail.com>
2023-06-30 19:37:09 +0530

    updated main

--- main.cpp
+++ main.cpp
@@ -2,29 +2,7 @@
 // Author: simranmakhijani55
 //
 
-#include <bits/stdc++.h>
-#define IOS                       \
-    ios_base::sync_with_stdio(0); \
-    cin.tie(0);                   \
-    cout.tie(0);
-#define ll long long int
-#define repn(i, m, n) for (ll i = m; i < n; i++)
-#define rep(i, n) repn(i, 0, n)
-#define pern(i, m, n) for (ll i = m; i >= n; i--)
-#define per(i, n) pern(i, n, 0)
-#define vl vector<ll>
-#define vvl vector<vl>
-#define v vector<int>
-#define vv vector<v>
-#define inpArr(arr, m, n) repn(i, m, n) cin >> arr[i];
-#define printArr(arr, m, n) repn(i, m, n) cout << arr[i] << ' ';
-#define cy cout << "YES\n"
-#define cn cout << "NO\n"
-#define nl '\n'
-#define pl pair<ll, ll>
-#define vb vector<bool>
-#define pb push_back
-#define ppb pop_back
+#include<simran.h>
 
 using namespace std;
 
